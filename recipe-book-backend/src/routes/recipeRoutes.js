188214831b3733f125b2d5582261f439de928ce6
jimbobding <jimmygrahamdavid@gmail.com>
2023-12-08 11:52:22 +0000

    adding route change

--- recipe-book-backend/src/routes/recipeRoutes.js
+++ recipe-book-backend/src/routes/recipeRoutes.js
@@ -1,10 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// router.post("/", recipeController.addRecipe);
-
-// module.exports = router;
-
 const express = require("express");
 const recipeController = require("../controllers/recipeController");
 
